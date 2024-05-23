790e13222e19a69c7124c2a3027d4e72b28951ee
mainvoid00 <yj050708@naver.com>
2024-05-23 15:04:04 +0900

    ver.1

---
+++ main.py
@@ -0,0 +1,36 @@
+import time
+
+#FCFS, SJF, 비선점 Priority, 선점 Priority, RR, SRT, HRN
+
+
+
+class scheduling() :
+    burst_time=[]
+    arrival_time=[]
+    wait_time=[]
+    PID=[]
+    
+    st
+    et
+    
+    
+def init():
+    pass
+
+
+def FCFS():
+    pass
+
+def SJF():
+    pass
+
+def Priority_Preemptive():
+    pass
+
+def Priority_Nonpreemptive():
+    pass
+
+
+
+def main():
+    pass
