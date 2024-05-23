04303d300c8be38da866cce80d7bc6b901d2c5b7
mainvoid00 <yj050708@naver.com>
2024-05-23 18:36:30 +0900

    ver1.2

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Scheduling() :
         self.PID = PID
         
     def Print(self):
-        print(self.PID[0], slef.PID[1])
+        print(self.PID[0], self.PID[1])
 
 
 def init(burst_time, arrival_time, PID, priority):
@@ -39,28 +39,38 @@ def init(burst_time, arrival_time, PID, priority):
             
             
             
-def FCFS(Scheduling):
-    pass
-            
+class FCFS(Scheduling):
+     
+    def __init__(self, burst_time, arrival_time, PID, priority):
+        super().__init__(burst_time, arrival_time, PID, priority)
+
+    n = len(self.PID)
+    processes = sorted(zip(self.arrival_time, self.burst_time, self.PID), key=lambda x: x[0])
+        
+    current_time = 0
+    waiting_time = []
+    turnaround_time = []
+        
+    for arrival, burst, pid in processes:
+        if current_time < arrival:
+                current_time = arrival
+        start_time = current_time
+        finish_time = start_time + burst
+        current_time = finish_time
+        waiting_time.append(start_time - arrival)
+        turnaround_time.append(finish_time - arrival)
+        
+    for i, (arrival, burst, pid) in enumerate(processes):
+            print(f"PID: {pid}, 도착 시간: {arrival}, 실행 시간: {burst}, 대기 시간: {waiting_time[i]}, 반환 시간: {turnaround_time[i]}")
+        
+    avg_waiting_time = sum(waiting_time) / n
+    avg_turnaround_time = sum(turnaround_time) / n
+        
+    print(f"평균 대기 시간: {avg_waiting_time}")
+    print(f"평균 반환 시간: {avg_turnaround_time}")
             
             
         
-        
-
-'''
-def FCFS():
-    pass
-
-def SJF():
-    pass
-
-def Priority_Preemptive():
-    pass
-
-def Priority_Nonpreemptive():
-    pass
-
-'''
 
 def main():
     burst_time=[]
@@ -69,8 +79,9 @@ def main():
     priority=[]
     init(burst_time, arrival_time, PID, priority)
     
-    scheduling = Scheduling(burst_time, arrival_time, PID, priority)
-    
+    scheduling_fcfs = FCFS(burst_time, arrival_time, PID, priority)
+    scheduling_fcfs.Print()
+    scheduling_fcfs.schedule()
     
     
     
