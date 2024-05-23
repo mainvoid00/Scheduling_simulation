169b5c6aec1ee3e82dd23e73cb1afcb7168b6628
mainvoid00 <yj050708@naver.com>
2024-05-23 17:07:47 +0900

    Super class create

--- main.py
+++ main.py
@@ -4,18 +4,50 @@ import time
 
 
 
-class scheduling() :
+class Scheduling() :
     burst_time=[]
+    priority=[]
     arrival_time=[]
-    wait_time=[]
     PID=[]
     
+    def __init__(self, burst_time, arrival_time, PID, priority):
+        self.burst_time = burst_time
+        self.arrival_time = arrival_time
+        self.priority= priority
+        self.PID = PID
+        
+    def Print(self):
+        print(self.PID[0], slef.PID[1])
+
+
+def init(burst_time, arrival_time, PID, priority):
     
-    
-def init():
+    with open("./init.txt", 'r') as f :
+        liens=[]
+        while True:
+            line = f.readline()
+            if not line:
+                break
+                
+            data= line.split(' ')
+            
+            PID.append(data[0])
+            burst_time.append(data[1])
+            arrival_time.append(data[2])
+            priority.append(data[3])
+            
+            
+            
+            
+def FCFS(Scheduling):
     pass
+            
+            
+            
+        
+        
 
-
+'''
 def FCFS():
     pass
 
@@ -28,11 +60,22 @@ def Priority_Preemptive():
 def Priority_Nonpreemptive():
     pass
 
-
+'''
 
 def main():
-    print("helloworld")
-    pass
+    burst_time=[]
+    arrival_time=[]
+    PID=[]
+    priority=[]
+    init(burst_time, arrival_time, PID, priority)
+    
+    scheduling = Scheduling(burst_time, arrival_time, PID, priority)
+    
+    
+    
+    
+    
+    
 
 
 if __name__ == "__main__":
