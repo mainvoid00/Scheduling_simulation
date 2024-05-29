281946640d57dcbcee1c9de53a391c8481727a84
mainvoid00 <yj050708@naver.com>
2024-05-29 19:56:06 +0900

    ver1.8

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import time
 import queue
 from tabulate import tabulate
-
-
+import matplotlib.pyplot as plt
+import pandas as pd
+ 
 
 #FCFS, SJF, 비선점 Priority, 선점 Priority, RR, SRT, HRN
 
@@ -14,12 +15,18 @@ class Scheduling() :
     arrival_time=[]
     PID=[]
     ready_queue=[]
+    
     DISPATCH=False
     def __init__(self, arrival_time, burst_time ,PID, priority):
         self.burst_time = (burst_time)
         self.arrival_time =(arrival_time)
         self.priority= (priority)
         self.PID = PID
+        self.start_time = [[] for _ in range(len(PID))]
+        self.end_time = [[] for _ in range(len(PID))]
+        self.wait_st = [[] for _ in range(len(PID))]
+        self.wait_ed = [[] for _ in range(len(PID))]
+
   
     def Print(self):
         print("-----Process Information-----")
@@ -73,16 +80,16 @@ def swap(arrival_time, burst_time, priority):
                 burst_time[j] = temp_burst_time
                 priority[j] = temp_priority
         
-                
-        
+
+
             
 class FCFS(Scheduling):
      
     def __init__(self, arrival_time, burst_time, PID, priority):
         super().__init__(arrival_time, burst_time, PID, priority)
-        self.st = time.time()
         self.pid_index = 0
         self.ready_queue = []
+        
         
     
     
@@ -91,21 +98,36 @@ class FCFS(Scheduling):
     def run(self):
 
         print("-----FCFS START-----")
+        self.st = time.time()
         
+        
+
+
+
         while True:
             now = time.time()
             print("now time=",int(now - self.st))
-            if self.pid_index < len(self.arrival_time) and int(now - self.st) >= self.arrival_time[self.pid_index]:
+
+            '''ready queue enqueue'''
+            if self.pid_index < len(self.arrival_time) and int(now - self.st) >= self.arrival_time[self.pid_index]: 
                 self.ready_queue.append(self.PID[self.pid_index])
                 print("PID", self.PID[self.pid_index], "enqueue")
+                self.wait_st[self.pid_index].append(int(now -self.st))
                 self.pid_index += 1
             
             print("ready_queue =", self.ready_queue)
             
+
+            '''ready_queue dequeue'''
             if self.DISPATCH == False and len(self.ready_queue) >0:
                 self.DISPATCH= True
                 dispatch_pid= self.ready_queue.pop(0)
                 dispatch_st = now
+                self.start_time[dispatch_pid].append(int(dispatch_st - self.st))
+                self.wait_ed[dispatch_pid].append(int(dispatch_st - self.st))
+
+                
+
 
                 
                 
@@ -116,11 +138,14 @@ class FCFS(Scheduling):
                 print("running pid=", dispatch_pid, "burst time=", int(now-dispatch_st) )
                 if(int(self.burst_time[dispatch_pid]-int(now-dispatch_st)) == 0):
                     print("time out PID",dispatch_pid)
+                    self.end_time[dispatch_pid].append(int(now-self.st))
                     self.DISPATCH = False
                     if len(self.ready_queue) > 0:
                         self.DISPATCH= True
                         dispatch_pid= self.ready_queue.pop(0)
                         dispatch_st = now  
+                        self.start_time[dispatch_pid].append(int(dispatch_st-self.st))
+                        self.wait_ed[dispatch_pid].append(int(dispatch_st - self.st))
 
                     
                 
@@ -132,9 +157,22 @@ class FCFS(Scheduling):
             
             
             time.sleep(1)
-            
+    ed= time.time()
 
+
+    def result(self,):
+        tasks=[]
+        for i in range(len(self.PID)):
+            print("----- Process ", self.PID[i], "-----")
+            for j in range(len(self.start_time[i])):
+                name= "P"+str(i)
+                tasks.append(dict(name, self.start_time[i][j], self.end_time[i][j]))
+                print(f"burst time = {self.end_time[i][j] - self.start_time[i][j]}")
+                print(f"wait time = {self.wait_ed[i][j] - self.wait_st[i][j]}")
         
+        
+
+
         
     
     
@@ -155,6 +193,7 @@ def main():
     scheduling_fcfs = FCFS(arrival_time, burst_time, PID, priority)
     scheduling_fcfs.Print()
     scheduling_fcfs.run()
+    scheduling_fcfs.result()
     
 
 
